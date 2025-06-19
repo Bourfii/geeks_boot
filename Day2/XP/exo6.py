b6355923a15dfc3d0034031c8d46c4e497ff1698
Assia <assiabourfii7@gmail.com>
2025-06-20 00:42:28 +0100

    add exo6

---
+++ Day2/XP/exo6.py
@@ -0,0 +1,8 @@
+def make_shirt(size ='large', text='I love Python'):
+    print(f"the size of this shirt is {size} and The message is  {text}")
+make_shirt("M","print()")    
+make_shirt()
+make_shirt("medium")
+make_shirt("small" ,"Geeks Intituate")
+#bonus
+make_shirt(size="small",text="Hello!")
